5b205a6ce18e6bf25e422f6a0afe22addd686ddc
raviranjan <raviranjancp@gmail.com>
2022-12-29 23:26:06 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react';
+import ReactDOM from 'react-dom';
+import './App.css'
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseForm from './components/ExpenseForm';
+import Title from './components/Title';
+
+const getItems = () =>{
+  let items = localStorage.getItem("items");
+  if(items === []){
+    return [];
+  }
+  else{
+    return JSON.parse(items);
+  }
+}
+
+const App = () => {
+  let i = 0;
+  const[newExpenseList, setExpenseList] = useState(getItems());
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const updatedExpenseList = [enteredExpenseData, ...newExpenseList];
+    setExpenseList(updatedExpenseList);
+  }
+
+  useEffect(()=>{
+    localStorage.setItem('items', JSON.stringify(newExpenseList))
+  },[newExpenseList]);
+
+  return (
+    <div className='app'>
+      <Title/>
+      <ExpenseForm
+        onSaveExpenseData = {saveExpenseDataHandler}
+      />
+      {
+        newExpenseList.map(
+          (expense) => (
+              <ExpenseItem
+              key = {i++}
+              date = {expense.date}
+              title = {expense.title}
+              amount = {expense.amount}
+          />
+        )
+      )
+    }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,34 @@
+
+import './ExpenseItem.css'
+
+const ExpenseItem = (props) => {
+    const date = new Date(props.date);
+    const month = date.toLocaleString('en-US', {month: 'long'});
+    const day = date.toLocaleString('en-US', {day: '2-digit'});
+    const year = date.getFullYear();
+    return (
+        <div className='expense-item'>
+            <div className='expense-item-container'>
+                <div className='expense-item-date'>
+                        <div className='expense-item-date-day'>{day}</div>
+                        <div className='expense-item-date-month'>{month}</div>
+                        <div className='expense-item-date-year'>{year}</div>
+                </div>
+                <div className='expense-item-title'>{props.title}
+                </div>
+                <div className='expense-item-amount'>Rs {props.amount}
+                </div>
+                <div className='icons'>
+                    <span class="material-symbols-outlined">
+                        delete
+                    </span>
+                    <span class="material-symbols-outlined">
+                        edit
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
